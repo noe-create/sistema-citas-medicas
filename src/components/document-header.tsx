ba8471d810e29205a59e59ae0aef12bb7953994c
Noe Peña <noej.p01@gmail.com>
2025-08-05 19:55:01 +0000

    SIGUE IGUAL NO ENTIENDO BUSCA LA CARPETA PUBLIC AHI ESTA LOS 2 LOGOS

--- src/components/document-header.tsx
+++ src/components/document-header.tsx
@@ -8,14 +8,14 @@ export function DocumentHeader() {
   return (
     <header className="flex justify-between items-center pb-2 border-b-2 border-black">
       <div className="w-1/4 flex justify-start">
-        <Image src="/logo_salud_integral.svg" alt="Salud Integral La Viña" width={100} height={60} />
+        <Image src="/logo_salud_integral.svg" alt="Salud Integral La Viña" width={120} height={60} />
       </div>
       <div className="w-1/2 text-center">
         <h1 className="text-xl font-bold tracking-wider">SALUD INTEGRAL</h1>
         <p className="text-xs">CENTRO POLITÉCNICO VALENCIA, C.A.</p>
       </div>
       <div className="w-1/4 flex justify-end">
-         <Image src="/logo_cpv.svg" alt="CPV Logo" width={50} height={50} />
+         <Image src="/logo_cpv.svg" alt="CPV Logo" width={60} height={40} />
       </div>
     </header>
   );
