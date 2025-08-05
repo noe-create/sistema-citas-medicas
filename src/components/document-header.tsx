5363c9bc77abb8a334d68c83ee1117118da66c10
Noe Peña <noej.p01@gmail.com>
2025-08-05 20:17:08 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/document-header.tsx
+++ src/components/document-header.tsx
@@ -2,20 +2,22 @@
 'use client';
 
 import * as React from 'react';
-import { ViñaIntegralLogo } from './viña-integral-logo';
+import Image from 'next/image';
 
 export function DocumentHeader() {
   return (
-    <header className="flex justify-center items-center text-center pb-2 border-b-2 border-black">
-      <div className="flex flex-col items-center">
-        <h1 className="text-xl font-bold tracking-wider">SALUD INTEGRAL</h1>
-        <div className="text-xs mt-1 space-y-0.5">
-            <p>CENTRO POLITÉCNICO VALENCIA, C.A.</p>
-            <p>Rif: J075055861 Nit: 0028937032</p>
-            <p>URB. LA VIÑA, FINAL AV. CARABOBO</p>
-            <p>Teléfonos: 0241 8268688 / 8268431 / 8202710</p>
-        </div>
-      </div>
-    </header>
+    <div className="relative">
+        <header className="flex justify-center items-center text-center pb-2 border-b-2 border-black">
+            <div className="flex flex-col items-center">
+                <h1 className="text-xl font-bold tracking-wider">SALUD INTEGRAL</h1>
+                <div className="text-xs mt-1 space-y-0.5">
+                    <p>CENTRO POLITÉCNICO VALENCIA, C.A.</p>
+                    <p>Rif: J075055861 Nit: 0028937032</p>
+                    <p>URB. LA VIÑA, FINAL AV. CARABOBO</p>
+                    <p>Teléfonos: 0241 8268688 / 8268431 / 8202710</p>
+                </div>
+            </div>
+        </header>
+    </div>
   );
 }
