add3aa3f09424a8fb84c0c492c35a021ab2118a6
Noe Peña <noej.p01@gmail.com>
2025-08-05 19:48:28 +0000

    QUIERO QUE INFORME TODO SE VEA COMO ESTABA ANTES OSEA ASI

--- src/components/document-header.tsx
+++ src/components/document-header.tsx
@@ -2,25 +2,20 @@
 'use client';
 
 import * as React from 'react';
-import { ViñaIntegralLogo } from './viña-integral-logo';
+import Image from 'next/image';
 
 export function DocumentHeader() {
   return (
     <header className="flex justify-between items-center pb-2 border-b-2 border-black">
       <div className="w-1/4 flex justify-start">
-        <ViñaIntegralLogo className="h-16 w-16" />
+        <Image src="/logo_salud_integral.svg" alt="Salud Integral La Viña" width={100} height={60} />
       </div>
       <div className="w-1/2 text-center">
-        <h1 className="text-2xl font-bold tracking-wider">Salud Integral</h1>
-        <div className="text-[10px] leading-tight mt-1">
-            <p className="font-bold">CENTRO POLICLINICO VALENCIA C.A</p>
-            <p>Rif.: J075055861 Nit.: 0028937032</p>
-            <p>URB. LA VIÑA, FINAL AV. CARABOBO</p>
-            <p>Teléfonos.: 0241 8268688 8268431 8202710</p>
-        </div>
+        <h1 className="text-xl font-bold tracking-wider">SALUD INTEGRAL</h1>
+        <p className="text-xs">CENTRO POLITÉCNICO VALENCIA, C.A.</p>
       </div>
       <div className="w-1/4 flex justify-end">
-         {/* Placeholder for the second logo */}
+         <Image src="/logo_cpv.svg" alt="CPV Logo" width={50} height={50} />
       </div>
     </header>
   );
