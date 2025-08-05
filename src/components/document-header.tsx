c66e695a6d821bd18d3ea7edefa353a175e856ea
Noe Peña <noej.p01@gmail.com>
2025-08-05 20:09:09 +0000

    ELIMINA ESOS 2 LOGOS QUE ESTAN EN LOS EXTREMOS DE LOS INFORMES

--- src/components/document-header.tsx
+++ src/components/document-header.tsx
@@ -2,20 +2,13 @@
 'use client';
 
 import * as React from 'react';
-import Image from 'next/image';
 
 export function DocumentHeader() {
   return (
-    <header className="flex justify-between items-center pb-2 border-b-2 border-black">
-      <div className="w-1/4 flex justify-start">
-        <Image src="/logo_salud_integral.svg" alt="Salud Integral La Viña" width={120} height={60} />
-      </div>
-      <div className="w-1/2 text-center">
+    <header className="flex justify-center items-center text-center pb-2 border-b-2 border-black">
+      <div>
         <h1 className="text-xl font-bold tracking-wider">SALUD INTEGRAL</h1>
         <p className="text-xs">CENTRO POLITÉCNICO VALENCIA, C.A.</p>
-      </div>
-      <div className="w-1/4 flex justify-end">
-         <Image src="/logo_cpv.svg" alt="CPV Logo" width={60} height={40} />
       </div>
     </header>
   );
