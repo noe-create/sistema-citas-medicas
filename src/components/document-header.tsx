8e702bf15451ab22dfb5daef591afda0967638d1
Noe Peña <noej.p01@gmail.com>
2025-08-05 20:10:41 +0000

    QUIERO QUE EN EL INFORME EN TODO EL CENTRO DE SALUD INTEGRAL  DEBAJO COL

--- src/components/document-header.tsx
+++ src/components/document-header.tsx
@@ -2,13 +2,19 @@
 'use client';
 
 import * as React from 'react';
+import { ViñaIntegralLogo } from './viña-integral-logo';
 
 export function DocumentHeader() {
   return (
     <header className="flex justify-center items-center text-center pb-2 border-b-2 border-black">
-      <div>
+      <div className="flex flex-col items-center">
         <h1 className="text-xl font-bold tracking-wider">SALUD INTEGRAL</h1>
-        <p className="text-xs">CENTRO POLITÉCNICO VALENCIA, C.A.</p>
+        <div className="text-xs mt-1 space-y-0.5">
+            <p>CENTRO POLITÉCNICO VALENCIA, C.A.</p>
+            <p>Rif: J075055861 Nit: 0028937032</p>
+            <p>URB. LA VIÑA, FINAL AV. CARABOBO</p>
+            <p>Teléfonos: 0241 8268688 / 8268431 / 8202710</p>
+        </div>
       </div>
     </header>
   );
