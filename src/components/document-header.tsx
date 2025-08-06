74327a0f33416f1315790b2e844a04fffa6293bb
Noe Peña <noej.p01@gmail.com>
2025-08-06 11:48:53 +0000

    QUIERO QUE ME ELIMINES ESOS 2 IMAGENES Q GENERASTES YO NO QUIERO ESAS IM

--- src/components/document-header.tsx
+++ src/components/document-header.tsx
@@ -1,27 +1,26 @@
 'use client';
 
 import * as React from 'react';
-import { LogoCPV } from './logo-cpv';
-import { LogoSaludIntegral } from './logo-salud-integral';
+import Image from 'next/image';
 
 export function DocumentHeader() {
   return (
     <header className="flex justify-between items-center text-center pb-2 border-b-2 border-black">
-        <div className="w-24 h-auto">
-             <LogoSaludIntegral />
+      <div className="w-24 h-auto">
+        <Image src="/logo_salud_integral.svg" alt="Logo Salud Integral" width={96} height={96} />
+      </div>
+      <div className="flex flex-col items-center flex-grow px-4">
+        <h1 className="text-xl font-bold tracking-wider">SALUD INTEGRAL</h1>
+        <div className="text-xs mt-1 space-y-0.5">
+          <p>CENTRO POLITÉCNICO VALENCIA, C.A.</p>
+          <p>Rif: J075055861 Nit: 0028937032</p>
+          <p>URB. LA VIÑA, FINAL AV. CARABOBO</p>
+          <p>Teléfonos: 0241 8268688 / 8268431 / 8202710</p>
         </div>
-        <div className="flex flex-col items-center flex-grow px-4">
-            <h1 className="text-xl font-bold tracking-wider">SALUD INTEGRAL</h1>
-            <div className="text-xs mt-1 space-y-0.5">
-                <p>CENTRO POLITÉCNICO VALENCIA, C.A.</p>
-                <p>Rif: J075055861 Nit: 0028937032</p>
-                <p>URB. LA VIÑA, FINAL AV. CARABOBO</p>
-                <p>Teléfonos: 0241 8268688 / 8268431 / 8202710</p>
-            </div>
-        </div>
-        <div className="w-24 h-auto">
-            <LogoCPV />
-        </div>
+      </div>
+      <div className="w-24 h-auto">
+        <Image src="/logo_cpv.svg" alt="Logo CPV" width={96} height={96} />
+      </div>
     </header>
   );
 }
