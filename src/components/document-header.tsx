7f82cb7449facc52f983a07f33b5f88e963a992e
Noe Peña <noej.p01@gmail.com>
2025-08-06 12:11:55 +0000

    AUN SIGUE IGUAL :/

--- src/components/document-header.tsx
+++ src/components/document-header.tsx
@@ -6,9 +6,7 @@ import * as React from 'react';
 export function DocumentHeader() {
   return (
     <header className="flex justify-between items-center text-center pb-2 border-b-2 border-black">
-      <div className="w-24 h-auto">
-        <img src="/logo_salud_integral.svg" alt="Logo Salud Integral" />
-      </div>
+      <img src="/logo_salud_integral.svg" alt="Logo Salud Integral" className="w-24 h-auto" />
       <div className="flex flex-col items-center flex-grow px-4">
         <h1 className="text-xl font-bold tracking-wider">SALUD INTEGRAL</h1>
         <div className="text-xs mt-1 space-y-0.5">
@@ -18,9 +16,7 @@ export function DocumentHeader() {
           <p>Teléfonos: 0241 8268688 / 8268431 / 8202710</p>
         </div>
       </div>
-      <div className="w-24 h-auto">
-        <img src="/logo_cpv.svg" alt="Logo CPV" />
-      </div>
+      <img src="/logo_cpv.svg" alt="Logo CPV" className="w-24 h-auto" />
     </header>
   );
 }
