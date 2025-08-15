5c1a783d69dc8b72ed502f02ab82514b0335276e
Noe Peña <noej.p01@gmail.com>
2025-08-15 00:05:50 +0000

    SSE VE IGUAL

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,11 +1,9 @@
-
 'use client';
 
 import * as React from 'react';
 import { useFormStatus } from 'react-dom';
 import { useActionState } from 'react';
 import { Button } from '@/components/ui/button';
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { login } from '@/actions/auth-actions';
